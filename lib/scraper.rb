3cfc2df690cb4b77d879807eaeb1597d092ee150
Kingsley <kingobaino@gmail.com>
2020-10-23 09:58:57 +0100

    Add scrape method

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,4 +8,18 @@ class Scraper
     @category = category
     @url = "https://www.nike.com/eg/w/#{gender}-#{category}-shoes#{ip_address}"
   end
+
+  def get_url(url)
+    unparsed_page = HTTParty.get(url)
+    Nokogiri::HTML(unparsed_page)
+  end
+
+  def parse_url
+    get_url(@url)
+  end
+
+  def scrape
+    product_items = parse_url.css('.product-grid__items').css('.product-card')
+    product_items.css('div.product-card__body').css('figure')
+  end
 end
