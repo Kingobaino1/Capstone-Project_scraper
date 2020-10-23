9daf7f6a01554f192a8555d6d9c0b59494599793
Kingsley <kingobaino@gmail.com>
2020-10-23 16:53:34 +0100

    Add test for get_url method

--- spec/scraper_spec.rb
+++ spec/scraper_spec.rb
@@ -4,8 +4,14 @@ describe Scraper do
   let(:scraper) { Scraper.new('mens', 'lifestyle', '-13jrmznik1zy7ok') }
   let(:url) { scraper.url }
 
+  describe '#get_url' do
+    it 'returns the path' do
+      expect(scraper.get_url(url)).not_to be nil
+    end
+  end
+
   describe '#scrape' do
-    it 'returns array of the path' do
+    it 'returns the path' do
       expect(scraper.scrape).not_to be nil
     end
   end
